6fa9ab0f850d757a7ab1b41f2b528aabc10d707e
gapigo <x.gabriel12@gmail.com>
2022-06-03 12:51:16 -0300

    Part 5 - Create Website Layout

---
+++ utils/class.js
@@ -0,0 +1,23 @@
+const classes = {
+  main: {
+    marginTop: 2,
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 1,
+    textAlign: 'center',
+  },
+  appbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 1,
+    },
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+};
+
+export default classes;
