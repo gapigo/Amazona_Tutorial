d628c3aebea7a58c461aa1e61e153a197b541fbc
gapigo <x.gabriel12@gmail.com>
2022-06-10 23:49:32 -0300

    Part 10 - Add to cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import client from '../../utils/client';
 import Layout from '../../components/Layout';
 import {
@@ -17,10 +17,18 @@ import {
 import NextLink from 'next/link';
 import classes from '../../utils/class';
 import Image from 'next/image';
-import { urlFor } from '../../utils/image';
+import { useSnackbar } from 'notistack';
+import { urlFor, urlForThumbnail } from '../../utils/image';
+import { Store } from '../../utils/store';
+import axios from 'axios';
 
 export default function ProductScreen(props) {
   const { slug } = props;
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(Store);
+  const { enqueueSnackbar } = useSnackbar();
   const [state, setState] = useState({
     product: null,
     loading: true,
@@ -43,6 +51,30 @@ export default function ProductScreen(props) {
     fetchData();
   }, []);
 
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      enqueueSnackbar('Sorry. Product is out of stock', { variant: 'error' });
+      return;
+    }
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {
+        _key: product._id,
+        name: product.name,
+        countInStock: product.countInStock,
+        slug: product.slug.current,
+        price: product.price,
+        image: urlForThumbnail(product.image),
+        quantity,
+      },
+    });
+    enqueueSnackbar(`${product.name} added to the cart`, {
+      variant: 'success',
+    });
+  };
   return (
     <Layout title={product?.title}>
       {loading ? (
@@ -116,7 +148,11 @@ export default function ProductScreen(props) {
                     </Grid>
                   </ListItem>
                   <ListItem>
-                    <Button fullWidth variant="contained">
+                    <Button
+                      onClick={addToCartHandler}
+                      fullWidth
+                      variant="contained"
+                    >
                       Add to cart
                     </Button>
                   </ListItem>
